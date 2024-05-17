d1491f64e295676c1fd561f1fe957da5cc1b7193
Lux Aliaga <lux@nixgoat.me>
2024-05-16 21:56:11 -0400

    test: Post type conversions
    Checks if the type conversions added to Post and Subpost work correctly.

--- src/test.rs
+++ src/test.rs
@@ -43,3 +43,40 @@ async fn fetch_nonexistent_post() {
     let resp = client.fetch_post("foo").await;
     assert!(resp.is_err());
 }
+
+#[tokio::test]
+async fn convert_to_post() {
+    let client = Threads::new().unwrap();
+    let child_resp = client.fetch_post("C6brVPxR1fZ").await;
+    println!("{:#?}", child_resp);
+    assert!(child_resp.is_ok());
+
+    let child_post = child_resp.unwrap();
+    println!("{:#?}", child_post);
+
+    let resp = child_post.parents[0].to_post().await;
+    println!("{:#?}", resp);
+    assert!(resp.is_ok());
+
+    let post = resp.unwrap();
+    assert_eq!(post.id, "3358445536292748283");
+    assert_eq!(post.name, "metaquest");
+}
+
+#[tokio::test]
+async fn convert_to_subpost() {
+    let client = Threads::new().unwrap();
+    let post_resp = client.fetch_post("C6brVPxR1fZ").await;
+    println!("{:#?}", post_resp);
+    assert!(post_resp.is_ok());
+
+    let post = post_resp.unwrap();
+    println!("{:#?}", post);
+
+    let resp = post.to_subpost(None).await;
+    println!("{:#?}", resp);
+    assert!(resp.is_ok());
+
+    let subpost = resp.unwrap();
+    assert_eq!(subpost.code, "C6brVPxR1fZ");
+}
